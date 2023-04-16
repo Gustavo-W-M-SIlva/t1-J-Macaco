6e8f2c20023af64306c647345623d097b4573ad0
GustavoWMSilva <willian.gustavo@edu.pucrs.br>
2023-04-16 14:03:25 -0300

    Array e HashMap, List: formados com sucesso

--- AppArrayHash.java
+++ AppArrayHash.java
@@ -4,12 +4,13 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.Arrays;
+import java.util.HashMap;
 
-public class App {
+// Array e HashMap
+public class ArrayHash {
     public static void main(String args[]) {
-
+        // para os testes foram executado um por vez
         executarThread("caso0050.txt", 50);
         executarThread("caso0100.txt", 100);
         executarThread("caso0200.txt", 200);
@@ -22,16 +23,22 @@ public class App {
     }
 
     public static void executarThread(String nomeArquivo, int jogo) {
+        //Para testar os casos de Array e do HashMap faça as devidas instruçoes de comentar e descomentar
+
         long tStart = System.currentTimeMillis();
+
         try (BufferedReader reader = Files.newBufferedReader(Paths.get(nomeArquivo), Charset.defaultCharset())) {
 
             String line = reader.readLine();
             String[] linha = line.split("\\s+");
 
+            // Padrao 1: Fazer (1) rodadas
             int rodadas = Integer.parseInt(linha[1]);
 
-            Macaco[] macacos = new Macaco[rodadas/100];
-            //HashMap<Integer, Macaco> macacos = new HashMap<>();
+            //Macaco[] macacos = new Macaco[rodadas/100]; // descomente
+             HashMap<Integer, Macaco> macacos = new HashMap<>(); // comente
+            
+            rodadas = rodadas/1000;// para o teste sem rodadas/1000, comente 
 
             while ((line = reader.readLine()) != null) {
                 linha = line.split("\\s+");
@@ -44,9 +51,9 @@ public class App {
                 int nPar = 0;
                 int nImp = 0;
 
-                for (int i = 11; i < 11 + tam; i++) { 
-                    int num = Character.getNumericValue(linha[i].charAt(linha[i].length() - 1));
-                    // num = Integer.parseInt(linha[i]);
+                for (int i = 11; i < 11 + tam; i++) {
+                    int num = Character.getNumericValue(linha[i].charAt(linha[i].length() - 1)); // descomente
+                    // num = Integer.parseInt(linha[i]); // comente
 
                     if (num % 2 == 0) {
                         nPar++;
@@ -55,20 +62,24 @@ public class App {
                     }
                 }
 
-                macacos[id] = new Macaco(id, envP, envI, nPar, nImp);
-                //macacos.put(id, new Macaco(id, envP, envI, nPar, nImp)); // adiciona o macaco à lista
+                //macacos[id] = new Macaco(id, envP, envI, nPar, nImp); // descomente
+                 macacos.put(id, new Macaco(id, envP, envI, nPar, nImp)); // comente
 
             }
             reader.close();
 
-            while (rodadas-- > 0) {
+           /* // descomente
+           while (rodadas-- > 0) { 
                 for (Macaco macaco : macacos) {
                     macacos[macaco.getEnvP()].addPar(macaco.removePar());
                     macacos[macaco.getEnvI()].addImp(macaco.removeImp());
                 }
             }
 
-            /*
+            // Ps. lambda tem seu charme
+            Macaco vencedor = Arrays.stream(macacos).max((a, b) -> a.getTam() - b.getTam()).orElse(null); //comente
+            */
+             // comente
               while (rodadas-- > 0) {
               for (Macaco macaco : macacos.values()) {
               macacos.get(macaco.getEnvP()).addPar(macaco.removePar());
@@ -76,7 +87,7 @@ public class App {
              }
               }
               
-              
+              // comente
               int maior = 0;
               int vencedor = 0;
               for (Integer id : macacos.keySet()) {
@@ -87,19 +98,17 @@ public class App {
               
               }
               }
-             */
+             
 
-            // Ps. lambda tem seu charme
-            Macaco vencedor = Arrays.stream(macacos).max((a, b) -> a.getTam() - b.getTam()).orElse(null);
+            
 
-            // System.out.println("\nNo jogo de caso " + jogo + " o vencedor foi o " + macacos.get(vencedor));
-            System.out.println("\nNo jogo de caso " + jogo + " o vencedor foi o " + vencedor);
+            System.out.println("\nNo jogo de caso " + jogo + " o vencedor foi o " + macacos.get(vencedor)); //comente
+            //System.out.println("\nNo jogo de caso " + jogo + " o vencedor foi o " + vencedor); //descomente
 
             System.out.println("O tempo de excução do jogo foi de = " + (System.currentTimeMillis() - tStart) + " ms.\n");
 
         } catch (IOException e) {
             System.err.format("Erro na leitura do arquivo: ", e);
         }
-    }// ).start();
+    }
 }
-// }
